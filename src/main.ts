649e33d11a6a116a8449c0c5939cfb996a006993
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-03-21 23:47:18 +0545

    feat: install and configure swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 1. Setup Swagger Config
+  const config = new DocumentBuilder()
+    .setTitle('Artist Management System')
+    .setDescription('API documentation for managing Users, Artists, and Music')
+    .setVersion('1.0')
+    .addBearerAuth() // This is for JWT later
+    .build();
+
+  // 2. Create Document
+  const document = SwaggerModule.createDocument(app, config);
+
+  // 3. Setup Swagger UI at /api
+  SwaggerModule.setup('api', app, document);
+
+  // Enable CORS so your React frontend can connect later
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
