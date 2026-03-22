ea0b87445fe8764c5af5e10a8a72424381ce5d26
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-03-22 14:34:38 +0545

    feat: update create and update dto

--- src/artist/dto/create-artist.dto.ts
+++ src/artist/dto/create-artist.dto.ts
@@ -1,1 +1,40 @@
-export class CreateArtistDto {}
+import {
+    IsString, IsOptional, IsEnum,
+    IsDateString, IsInt, Min, IsUUID,
+} from 'class-validator';
+
+export enum GenderType {
+    MALE = 'm',
+    FEMALE = 'f',
+    OTHER = 'o',
+}
+export class CreateArtistDto {
+    @IsOptional()
+    @IsUUID()
+    user_id?: string;
+
+    @IsString()
+    name: string;
+
+    @IsOptional()
+  @IsDateString()
+  dob?: string;
+
+  @IsOptional()
+  @IsEnum(GenderType)
+  gender?: GenderType;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1900)
+  first_release_year?: number;
+
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  no_of_albums_released?: number;
+}
--- src/artist/dto/update-artist.dto.ts
+++ src/artist/dto/update-artist.dto.ts
@@ -1,4 +1,7 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateArtistDto } from './create-artist.dto';
 
-export class UpdateArtistDto extends PartialType(CreateArtistDto) {}
+export class UpdateArtistDto extends PartialType(CreateArtistDto) {
+
+    
+}
