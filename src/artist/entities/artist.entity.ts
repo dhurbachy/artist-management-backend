f22ca9947682ea32794cbd2293880e7b60f631f7
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-03-22 14:34:18 +0545

    feat: update artist entity

--- src/artist/entities/artist.entity.ts
+++ src/artist/entities/artist.entity.ts
@@ -1,1 +1,12 @@
-export class Artist {}
+export interface Artist {
+  id:                    string;
+  user_id:               string | null;
+  name:                  string;
+  dob:                   Date   | null;
+  gender:                'm' | 'f' | 'o' | null;
+  address:               string | null;
+  first_release_year:    number | null;
+  no_of_albums_released: number;
+  created_at:            Date;
+  updated_at:            Date;
+}
