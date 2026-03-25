8eb97abd595d92f0670db513f6f438d34200fd78
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-03-25 15:51:18 +0545

    chores: handle cors error on specific url

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,12 @@ async function bootstrap() {
     const app    = await NestFactory.create(AppModule);
     const logger = new Logger('Bootstrap');
 
-    app.enableCors();
-
+app.enableCors({
+    origin: "http://localhost:5173", 
+    credentials: true,               
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+});
     app.setGlobalPrefix('api', {
       exclude: ['docs', 'docs-json'],
     });
