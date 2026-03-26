a829086989685834f7b9014075e1428f60735c0a
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-03-26 08:16:37 +0545

    fix: validation expand

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,15 +1,18 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, MinLength, IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 
 export class LoginDto {
   @ApiProperty({ example: "test@example.com" })
-
+  @Transform(({value})=>value.trim())
   @IsEmail()
+  @IsNotEmpty()
   email: string;
-  
-  @ApiProperty({ minLength: 8, example: "password123" })
 
+  @ApiProperty({ minLength: 8, example: "password123" })
+  @Transform(({value})=>value.trim())
   @IsString()
   @MinLength(8)
+  @IsNotEmpty()
   password: string;
 }
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,42 +1,52 @@
 import {
   IsEmail, IsString, MinLength,
-  IsOptional, IsEnum, IsDateString,
+  IsOptional, IsEnum, IsDateString, IsNotEmpty, Matches
 } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-
+import { Transform } from 'class-transformer';
 export enum UserRole {
-  SUPER_ADMIN    = 'super_admin',
+  SUPER_ADMIN = 'super_admin',
   ARTIST_MANAGER = 'artist_manager',
-  ARTIST         = 'artist',
+  ARTIST = 'artist',
 }
 
 export enum GenderType {
-  MALE   = 'm',
+  MALE = 'm',
   FEMALE = 'f',
-  OTHER  = 'o',
+  OTHER = 'o',
 }
 
 export class RegisterDto {
   @ApiProperty({ example: 'John' })
   @IsString()
+  @IsNotEmpty()
+  @Transform(({ value }) => value.trim())
   first_name: string;
 
   @ApiProperty({ example: 'Doe' })
   @IsString()
+  @IsNotEmpty()
+  @Transform(({ value }) => value.trim())
   last_name: string;
 
   @ApiProperty({ example: 'john@example.com' })
   @IsEmail()
+  @Transform(({ value }) => value.trim())
+
   email: string;
 
   @ApiProperty({ minLength: 8, example: 'password123' })
   @IsString()
   @MinLength(8)
+  @IsNotEmpty()
   password: string;
 
   @ApiPropertyOptional({ example: '+9779812345678' })
   @IsOptional()
   @IsString()
+  @Matches(/^\+?[0-9]{7,15}$/, {
+    message: 'Invalid phone number',
+  })
   phone?: string;
 
   @ApiPropertyOptional({ example: '1990-01-01' })
