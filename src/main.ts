57c4f4ca72aa16db1619bd2f98ff9936409c45d8
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-04-21 09:00:40 +0545

    update

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,29 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { runMigrations } from './migrations/runner';
+import { DatabaseService } from './core/database/database.service';
 
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
     const logger = new Logger('Bootstrap');
+    const dbService = app.get(DatabaseService);
+    logger.log('Checking for pending migrations...');
+    if (typeof dbService.onModuleInit === 'function') {
+      await dbService.onModuleInit();
+    }
+    await runMigrations(dbService);
     app.use(cookieParser());
     app.enableCors({
-      origin: "http://localhost:5173",
+      origin: 'http://localhost:5173',
       credentials: true,
-      methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
-      allowedHeaders: ["Content-Type", "Authorization"],
+      methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
     });
     app.setGlobalPrefix('api', {
       exclude: ['docs', 'docs-json'],
     });
-
 
     app.useGlobalPipes(
       new ValidationPipe({
@@ -35,10 +42,12 @@ async function bootstrap() {
       }),
     );
 
-    // Swagger 
+    // Swagger
     const config = new DocumentBuilder()
       .setTitle('Artist Management System')
-      .setDescription('API documentation for managing Users, Artists, and Music')
+      .setDescription(
+        'API documentation for managing Users, Artists, and Music',
+      )
       .setVersion('1.0')
       .addBearerAuth(
         {
@@ -61,7 +70,6 @@ async function bootstrap() {
     await app.listen(process.env.PORT ?? 3000);
     logger.log(`http://localhost:${process.env.PORT ?? 3000}/api/v1`);
     logger.log(`http://localhost:${process.env.PORT ?? 3000}/docs`);
-
   } catch (err) {
     console.error('Bootstrap failed:', err.message);
     process.exit(1);
